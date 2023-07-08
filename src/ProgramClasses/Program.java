159b6c9d2b9fd0b8badb173388c4876d9d61ab36
Asu21244 <hawa21244@gmail.com>
2023-07-08 00:29:03 -0400

    push my guy

--- src/ProgramClasses/Program.java
+++ src/ProgramClasses/Program.java
@@ -1,4 +1,5 @@
 package ProgramClasses;
+
 import RemainingClasses.Course;
 
 public class Program {
@@ -7,41 +8,53 @@ public class Program {
 	int totalCredits;
 	String institution;
 	Course[] requiredCourses;
-	
+
 	public Program() {
-		name=null;
-		specialization=null;
-		totalCredits=0;
-		institution=null;
-		
+		name = null;
+		specialization = null;
+		totalCredits = 0;
+		institution = null;
+
 	}
+
 	public Program() {
-		
+
 	}
+
 	public Program(Program program1) {
-		name=program1.name;
-		specialization=program1.specialization;
-		totalCredits=program1.totalCredits;
-		institution=program1.institution;
-		
+		name = program1.name;
+		specialization = program1.specialization;
+		totalCredits = program1.totalCredits;
+		institution = program1.institution;
+
 	}
-	
+
 	public void setName(String name) {
-		this.name=name;
+		this.name = name;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setSpecialization(String specialization) {
-		this.specialization=specialization;
+		this.specialization = specialization;
 	}
+
 	public String getSpecialization() {
 		return specialization;
 	}
+
 	public void setTotalCredits(int totalCredits) {
-		this.totalCredits=totalCredits;
+		this.totalCredits = totalCredits;
 	}
+
 	public int getTotalCredits() {
 		return totalCredits;
+	}
+
+	// to modify.
+	public String toString() {
+		return name;
 	}
 }
