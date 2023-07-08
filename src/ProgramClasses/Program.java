58c10d47fada4066a9f9061ed110b53401331a83
Asu21244 <hawa21244@gmail.com>
2023-07-08 12:07:17 -0400

    added comments

--- src/ProgramClasses/Program.java
+++ src/ProgramClasses/Program.java
@@ -10,19 +10,19 @@ public class Program {
 	Course[] requiredCourses;
 	Course[] electiveCourses;
 
-	public Program() {
+	public Program() { //Default constructor
 		name = null;
 		specialization = null;
 		totalCredits = 0;
 		institution = null;
 
 	}
+	
+	//public Program() { //Custom constructor
 
-	public Program() {
+	//}
 
-	}
-
-	public Program(Program program1) {
+	public Program(Program program1) { //Copy constructor
 		name = program1.name;
 		specialization = program1.specialization;
 		totalCredits = program1.totalCredits;
@@ -30,32 +30,32 @@ public class Program {
 
 	}
 
-	public void setName(String name) {
+	public void setName(String name) { //Setter method for the String name
 		this.name = name;
 	}
 
-	public String getName() {
+	public String getName() { //Getter method for the String name
 		return name;
 	}
 
-	public void setSpecialization(String specialization) {
+	public void setSpecialization(String specialization) { //Setter method for the String specialization
 		this.specialization = specialization;
 	}
 
-	public String getSpecialization() {
+	public String getSpecialization() { //Getter method for the String specialization
 		return specialization;
 	}
 
-	public void setTotalCredits(int totalCredits) {
+	public void setTotalCredits(int totalCredits) { //Setter method for the integer totalCredits
 		this.totalCredits = totalCredits;
 	}
 
-	public int getTotalCredits() {
+	public int getTotalCredits() { //Getter method for the integer totalCredits
 		return totalCredits;
 	}
 
 	// to modify.
-	public String toString() {
+	public String toString() { //ToString method
 		return name;
 	}
 }
