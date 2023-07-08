15d2c6b3080542b13601dfd371dc87d5adf9044a
Asu21244 <hawa21244@gmail.com>
2023-07-07 23:46:55 -0400

    creation of new methods
    Methods: - getter and setter for name
                    - getter and setter for specialization

--- src/ProgramClasses/Program.java
+++ src/ProgramClasses/Program.java
@@ -7,4 +7,17 @@ public class Program {
 	String institution;
 	int idk;
 	
+	public void setName(String name) {
+		this.name=name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setSpecialization(String specialization) {
+		this.specialization=specialization;
+	}
+	public String getSpecialization() {
+		return specialization;
+	}
+	public void 
 }
