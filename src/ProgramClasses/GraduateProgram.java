5699f27b8cfd37e03fcfd4451b89f3ebebbe0941
Asu21244 <hawa21244@gmail.com>
2023-07-10 20:33:53 -0400

    All constructors

--- src/ProgramClasses/GraduateProgram.java
+++ src/ProgramClasses/GraduateProgram.java
@@ -5,5 +5,17 @@ public class GraduateProgram extends Program{
 	String coordinator; 
 	static int creditCost;
 	
+	public GraduateProgram() {
+		coordinator=null;
+		creditCost=0;
+	}
+	public GraduateProgram(String coordinator,int creditCost) {
+		this.coordinator=coordinator;
+		GraduateProgram.creditCost=creditCost;
+	}
+	public GraduateProgram(GraduateProgram graduate) {
+		this.coordinator=graduate.coordinator;
+		GraduateProgram.creditCost=graduate.creditCost;
+	}
 
 }
--- src/ProgramClasses/Program.java
+++ src/ProgramClasses/Program.java
@@ -1,4 +1,5 @@
 package ProgramClasses;
+
 import RemainingClasses.Course;
 
 public class Program {
@@ -9,19 +10,25 @@ public class Program {
 	Course[] requiredCourses;
 	Course[] electiveCourses;
 
-	public Program() { //Default constructor
+	public Program() { // Default constructor
 		name = null;
 		specialization = null;
 		totalCredits = 0;
 		institution = null;
 
 	}
-	
-	//public Program() { //Custom constructor
 
-	//}
+	public Program(String name, String specialization, int totalCredits, String institution, Course[] requiredCourses,
+			Course[] electiveCourses) { // Custom constructor
+		this.name = name;
+		this.specialization = specialization;
+		this.totalCredits = totalCredits;
+		this.institution = institution;
+		this.requiredCourses = requiredCourses;
+		this.electiveCourses = electiveCourses;
+	}
 
-	public Program(Program program1) { //Copy constructor
+	public Program(Program program1) { // Copy constructor
 		name = program1.name;
 		specialization = program1.specialization;
 		totalCredits = program1.totalCredits;
@@ -29,32 +36,32 @@ public class Program {
 
 	}
 
-	public void setName(String name) { //Setter method for the String name
+	public void setName(String name) { // Setter method for the String name
 		this.name = name;
 	}
 
-	public String getName() { //Getter method for the String name
+	public String getName() { // Getter method for the String name
 		return name;
 	}
 
-	public void setSpecialization(String specialization) { //Setter method for the String specialization
+	public void setSpecialization(String specialization) { // Setter method for the String specialization
 		this.specialization = specialization;
 	}
 
-	public String getSpecialization() { //Getter method for the String specialization
+	public String getSpecialization() { // Getter method for the String specialization
 		return specialization;
 	}
 
-	public void setTotalCredits(int totalCredits) { //Setter method for the integer totalCredits
+	public void setTotalCredits(int totalCredits) { // Setter method for the integer totalCredits
 		this.totalCredits = totalCredits;
 	}
 
-	public int getTotalCredits() { //Getter method for the integer totalCredits
+	public int getTotalCredits() { // Getter method for the integer totalCredits
 		return totalCredits;
 	}
 
 	// to modify.
-	public String toString() { //ToString method
+	public String toString() { // ToString method
 		return name;
 	}
 }
