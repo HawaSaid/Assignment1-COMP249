dc4c1f798c3244e225c6598aec69474b914fbc8b
Asu21244 <hawa21244@gmail.com>
2023-07-13 22:34:48 -0400

    copyTheObjects method completed

--- src/ProgramClasses/NewDriver.java
+++ src/ProgramClasses/NewDriver.java
@@ -6,18 +6,24 @@ package ProgramClasses;
 //Written by: Hawa-Afnane Said(ID:40263400)
 //------------------------------------------------------------------------------
 /**
-*
-* @author Hawa-Afnane Said
-*/
+ *
+ * @author Hawa-Afnane Said
+ */
 
 public class NewDriver {
 
-	public static void copyTheObjects(Program[] array) {
+	public static Program[] copyTheObjects(Program[] array) {// Custom method that creates a copy of the array were
+																// passing
+		// through the method
 		Program[] arrcopy = new Program[array.length];
 		for (int j = 0; j < array.length; j++) {
-		
-
+			if (arrcopy[j] instanceof Program) {
+				arrcopy[j] = new Program(array[j]);
+			} else {
+				System.out.println("Can't find a Program object");
+			}
 		}
+		return arrcopy;
 
 	}
 
@@ -26,4 +32,3 @@ public class NewDriver {
 	}
 
 }
-
