a65836baa361e6691a4a022106155ef589508b67
Asu21244 <hawa21244@gmail.com>
2023-07-13 23:04:02 -0400

    SpecialProgram Class

--- src/RemainingClasses/SpecialProgram.java
+++ src/RemainingClasses/SpecialProgram.java
@@ -1,24 +1,58 @@
 package RemainingClasses;
+
 import ProgramClasses.Program;
 
 public class SpecialProgram extends Program {
-
 	int duration;
 	static int creditCost;
 
-	public SpecialProgram() {
+	public SpecialProgram() {// Default constructor
 		duration = 0;
 		creditCost = 0;
 	}
 
-	public SpecialProgram(int capacity, int creditCost) {
+	public SpecialProgram(int capacity, int creditCost) {// Custom constructor
 		this.duration = capacity;
 		SpecialProgram.creditCost = creditCost;
 	}
 
-	public SpecialProgram(CertificateProgram certificate) {
+	public SpecialProgram(CertificateProgram certificate) {// Copy constructor
+		super(certificate);
 		this.duration = certificate.capacity;
-		SpecialProgram.creditCost = certificate.creditCost;
+		SpecialProgram.creditCost = certificate.getCreditCost();
+	}
+
+	public void setDuration(int duration) {// Setter method for the int duration
+		this.duration = duration;
+	}
+
+	public int getDuration() {// Getter method for the int duration
+		return duration;
+	}
+
+	public void setCreditCost(int creditCost) {// Setter method for the int creditCost
+		GraduateProgram.creditCost = creditCost;
+	}
+
+	public int getCreditCost() {// Getter method for the int creditCost
+		return creditCost;
+	}
+
+	@Override
+	public String toString() {// toString method
+		return "The duration is for the SpecialProgram " + duration + " and the creditCost is" + creditCost;
+	}
+
+	@Override
+	public boolean equals(Program sp) { // Equals method
+		if (sp == null || getClass() != sp.getClass()) {
+			return false;
+		} else if (this == sp) {
+			return true;
+		}
+		SpecialProgram othersp = (SpecialProgram) sp;
+		return this.duration == othersp.duration && SpecialProgram.creditCost == othersp.getCreditCost();
+
 	}
 
 }
