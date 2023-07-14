213f4b13b0bc3d3b246a1d97ce974d65fbabf589
HaifaaJanoudi <138940955+HaifaaJanoudi@users.noreply.github.com>
2023-07-13 22:49:43 -0400

    Update Program.java

--- src/ProgramClasses/Program.java
+++ src/ProgramClasses/Program.java
@@ -11,10 +11,10 @@ public class Program {
 	Course[] electiveCourses;
 
 	public Program() { // Default constructor
-		name = null;
-		specialization = null;
-		totalCredits = 0;
-		institution = null;
+		this.name = null;
+		this.specialization = null;
+		this.totalCredits = 0;
+		this.institution = null;
 
 	}
 
@@ -29,10 +29,10 @@ public class Program {
 	}
 
 	public Program(Program program1) { // Copy constructor
-		name = program1.name;
-		specialization = program1.specialization;
-		totalCredits = program1.totalCredits;
-		institution = program1.institution;
+		this.name = program1.name;
+		this.specialization = program1.specialization;
+		this.totalCredits = program1.totalCredits;
+		this.institution = program1.institution;
 
 	}
 
@@ -41,7 +41,7 @@ public class Program {
 	}
 
 	public String getName() { // Getter method for the String name
-		return name;
+		return this.name;
 	}
 
 	public void setSpecialization(String specialization) { // Setter method for the String specialization
@@ -49,7 +49,7 @@ public class Program {
 	}
 
 	public String getSpecialization() { // Getter method for the String specialization
-		return specialization;
+		return this.specialization;
 	}
 
 	public void setTotalCredits(int totalCredits) { // Setter method for the integer totalCredits
@@ -57,11 +57,53 @@ public class Program {
 	}
 
 	public int getTotalCredits() { // Getter method for the integer totalCredits
-		return totalCredits;
+		return this.totalCredits;
 	}
-
-	// to modify.
-	public String toString() { // ToString method
-		return name;
+	public void setrequiredCourses(Course[] requiredCourses) { //setter method for the array required courses
+		this.requiredCourses = requiredCourses;
+		
 	}
+	public Course[] getrequiredCourses() { //getter method for the required courses
+		return this.requiredCourses;
+	}
+	public void setelectiveCourses(Course[] electiveCourses) { //setter method for the array elective courses
+		this.electiveCourses = electiveCourses;
+	}
+	public Course[] getelectiveCourses() { //getter method for the array elective courses
+		return this.electiveCourses; 
+	}
+	public void setinstituiton() { //setter method for the string institution 
+		this.institution = institution;
+	}
+	public String getinstitution() { //getter method for the string institution
+		return this.institution;
+	}
+	
+	//overridden methods 
+	public String toString() {
+		return this.institution + "offer the program of " + this.name "the the following specializaition" + this.specialization + "that include the following courses: \n" 
+				+ this.requiredCourses + " are the required courses \n "
+				+ this.electiveCourses + " are the elective courses \n"
+				+ " and the total amount of credits is " + this.totalCredits + "credits."
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
