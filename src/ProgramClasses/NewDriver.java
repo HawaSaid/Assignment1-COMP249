f8e17eb2d6f53778d794df794fd5a8592e9b9eaa
Asu21244 <hawa21244@gmail.com>
2023-07-13 21:24:42 -0400

    NewDriver class for part2

---
+++ src/ProgramClasses/NewDriver.java
@@ -0,0 +1,29 @@
+package ProgramClasses;
+
+//------------------------------------------------------------------------------
+//Assignment 1
+//Part: Part 2
+//Written by: Hawa-Afnane Said(ID:40263400)
+//------------------------------------------------------------------------------
+/**
+*
+* @author Hawa-Afnane Said
+*/
+
+public class NewDriver {
+
+	public static void copyTheObjects(Program[] array) {
+		Program[] arrcopy = new Program[array.length];
+		for (int j = 0; j < array.length; j++) {
+		
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
